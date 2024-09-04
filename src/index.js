2353ddb9f0a2a127bcb2ad6404c7574d568898ff
Pratham Jain <2muchappy420@gmail.com>
2024-09-04 17:58:15 +0530

    Database successfully connected

---
+++ src/index.js
@@ -0,0 +1,41 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/db.js";
+
+dotenv.config({
+    path: './env'
+})
+
+
+
+connectDB()
+
+
+
+
+
+
+// import express from "express"
+// const app = express()
+
+// ;( async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("errror", (error) => {
+//             console.log("The app is not able to talk to the database", error);
+//             throw err;
+//         })
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is listening on port ${process.env.PORT}`);
+            
+//         })
+
+//     } catch (error) {
+//         console.error("ERROR : ", error);
+//         throw err        
+//     }
+// })()
