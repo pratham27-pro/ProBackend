03c208552821318c60999b792049259fe5e37135
Pratham Jain <2muchappy420@gmail.com>
2024-09-07 23:07:51 +0530

    APIs handled and user registration made

--- src/app.js
+++ src/app.js
@@ -14,5 +14,19 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// routes
+import userRouter from "./routes/user.routes.js";
+
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
+
 
 export {app}
+
+
+/*
+1. ek form bana do jisko user bharde
+2. uss form ka data database mei save karlo
+3. 
+*/
